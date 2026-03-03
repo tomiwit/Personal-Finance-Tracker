40c6fbcda57aa22da359b58570573425aa09e715
Thomas Pogg <tomegg0@gmail.com>
2026-03-03 22:24:33 +0100

    Update Database script

--- projpy.py
+++ projpy.py
@@ -6,7 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()
 db_path = os.getenv("SQLITE_PATH")
 
-print(db_path)
 
 connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
+
+create_table_wydatki = """CREATE TABLE wydatki 
+                            (id_wydatku INTEGER PRIMARY KEY AUTOINCREMENT,
+                             data TEXT,
+                             tytul_operacji TEXT,
+                             odbiorca TEXT,
+                             id_kategorii INTEGER,
+                             kwota REAL,
+                             FOREIGN KEY (id_kategorii) REFERENCES kategorie (id_kategorii)) """
+
+create_table_kategorie = """CREATE TABLE kategorie (
+                                id_kategorii INTEGER PRIMARY KEY AUTOINCREMENT,
+                                nazwa_kategorii TEXT) """
+
+
+cursor.execute(create_table_kategorie)
+cursor.execute(create_table_wydatki)
+connection.commit()
+
