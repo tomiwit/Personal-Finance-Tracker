7ed221f59f9e9862c9e5510b11989cd2157c9b2b
Thomas Pogg <tomegg0@gmail.com>
2026-03-09 17:22:13 +0100

    Created categorization function with genai library

--- projpy.py
+++ projpy.py
@@ -3,11 +3,50 @@ import os
 import pandas as pd
 from pathlib import Path
 from dotenv import load_dotenv
+from google import genai
+import json
+
+def get_transaction_data(client, raw_text):
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        config = genai.types.GenerateContentConfig(
+            system_instruction="""Jesteś asystentem do analizy wydatków bankowych.
+            Twoim zadaniem jest najpierw odczytanie z tytułu operacji odbiory (np zabka, kaufland, szpital carolina),
+            a następnie dopasowanie kategorii.
+            Na wejściu dostajesz tytuł operacji bankowej.
+            Odpowiadasz TYLKO w formacie JSON, na wyjsciu oczekuję:
+            {"odbiorca": "nazwa", "kategoria": "kategoria"}
+            Kategorie to: jedzenie, transport, rozrywka, zdrowie, mieszkanie, ubrania, inne, blik (uzywaj tego w przypadku przelewu na telefon) ."""),
+            contents = raw_text)
+    clean = response.text.strip().replace("```json", "").replace("```", "").strip()
+    
+    result = json.loads(clean)
+    return result["odbiorca"], result["kategoria"]
+     
+
+def create_tables(cursor):
+    cursor.execute( """CREATE TABLE IF NOT EXISTS wydatki 
+                                (id_wydatku INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 data TEXT,
+                                 tytul_operacji TEXT,
+                                 odbiorca TEXT,
+                                 id_kategorii INTEGER,
+                                 kwota REAL,
+                                 FOREIGN KEY (id_kategorii) REFERENCES kategorie (id_kategorii)) """)
+                                
+    cursor.execute("""CREATE TABLE IF NOT EXISTS kategorie (
+                                    id_kategorii INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    nazwa_kategorii TEXT) """)
+
 
 
 load_dotenv(override=True)
 db_path = os.getenv("SQLITE_PATH")
 raw_path_data = os.getenv("CSV_FOLDER")
+google_api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=google_api_key)
+
 
 if raw_path_data:
     csv_path = Path(raw_path_data)
@@ -17,28 +56,17 @@ else:
     
 connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
+create_tables(cursor)
+connection.commit()
 
-create_table_wydatki = """CREATE TABLE wydatki 
-                            (id_wydatku INTEGER PRIMARY KEY AUTOINCREMENT,
-                             data TEXT,
-                             tytul_operacji TEXT,
-                             odbiorca TEXT,
-                             id_kategorii INTEGER,
-                             kwota REAL,
-                             FOREIGN KEY (id_kategorii) REFERENCES kategorie (id_kategorii)) """
-
-create_table_kategorie = """CREATE TABLE kategorie (
-                                id_kategorii INTEGER PRIMARY KEY AUTOINCREMENT,
-                                nazwa_kategorii TEXT) """
 
 columns_raw_data = [
     'Data operacji', 'Data ksiegowania', 'Tytul operacji', 
     'Dane strony operacji', 'Rachunek strony operacji', 
     'Kwota', 'Saldo', 'Waluta', 'Inne'
 ]
-cursor.execute(create_table_kategorie)
-cursor.execute(create_table_wydatki)
-connection.commit()
+
+
 
 raw_data = pd.read_csv(
     csv_path,
@@ -49,3 +77,4 @@ raw_data = pd.read_csv(
 )
 
 raw_data.columns = columns_raw_data
+
