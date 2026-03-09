95d1de3c793ca4c849cb79ecccae272717dcf01d
Thomas Pogg <tomegg0@gmail.com>
2026-03-09 23:18:45 +0100

    tested function and built loop that inserts data to tables

--- projpy.py
+++ projpy.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import pandas as pd
+import random
 from pathlib import Path
 from dotenv import load_dotenv
 from google import genai
@@ -78,3 +79,27 @@ raw_data = pd.read_csv(
 
 raw_data.columns = columns_raw_data
 
+sample = raw_data['Tytul operacji'].sample(10,random_state = 42)
+
+for tytul in sample:
+    print(f"Nazwa operacji: {tytul}")
+    odbiorca_test, kategoria_test  = get_transaction_data(client, tytul)
+    print(f"Odbiorca: {odbiorca_test} ||| Kategoria: {kategoria_test}\n")
+    
+    
+for index,row in raw_data.iterrows():
+    odbiorca, kategoria = get_transaction_data(client, row['Tytul operacji'])
+    cursor.execute("SELECT id_kategorii, nazwa_kategorii from kategorie where nazwa_kategorii = (?)",(kategoria,))
+    result = cursor.fetchone()
+    if result:
+        id_kat = result[0]
+    else:
+        cursor.execute("INSERT INTO kategorie (nazwa_kategorii) VALUES (?)", (kategoria,))
+        connection.commit()
+        cursor.execute("SELECT id_kategorii FROM kategorie order by id_kategorii DESC LIMIT 1")
+        result = cursor.fetchone()
+        id_kat = result[0]
+    cursor.execute("INSERT INTO wydatki (data,tytul_operacji,odbiorca,id_kategorii,kwota) VALUES (?,?,?,?,?)",
+                   (row['Data ksiegowania'],row['Tytul operacji'],odbiorca,id_kat,row['Kwota'], ))
+    connection.commit()
+        
